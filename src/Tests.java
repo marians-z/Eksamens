8f2091d700e237195f594281e5924083a974f7c3
marians <marians15150@gmail.com>
2021-06-17 17:53:54 +0300

    pirmais jautajums

--- src/Tests.java
+++ src/Tests.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class Tests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner dati=new Scanner(System.in);
+		String[] jautajumi = {"Tests1\n", "Tests2\n", "Tests3\n", "Tests\n", "Tests5\n", "Tests6\n", "Tests7\n", "Tests8\n", "Tests9\n", "Tests10\n",};
+		String atbildes = "";
+		atbildes=atbildes+jautajumi[0]+jautajumi[1];
+		System.out.println(atbildes);
 
 	}
 
